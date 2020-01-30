a083e2744116c761779a5da3468644ff805f149a
Jose Mamani <JOSEMLI@example.com>
2020-01-30 11:48:45 -0500

    terminado la parte de pedir parte del cliente

--- app/src/main/java/com/example/android_u2_tema2_micliente/MainActivity.java
+++ app/src/main/java/com/example/android_u2_tema2_micliente/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,7 +15,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.socket.client.Ack;
+import io.socket.client.Socket;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+  Socket mSocket;
   LatLng pos;
   GoogleMap mapa;
   @Override
@@ -23,6 +33,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     SupportMapFragment mapFragment = (SupportMapFragment)
         getSupportFragmentManager().findFragmentById(R.id.mapa);
     mapFragment.getMapAsync(this);
+
+    mSocket = App.getSocket();
+//mSocket.on("taxiencontrado", taxiencontrado);
+//mSocket.on("localizacion",localizacion);
+//mSocket.on("Abordo",abordo);
+    mSocket.connect();
+
   }
   @Override
   public void onMapReady(GoogleMap googleMap) {
@@ -38,5 +55,24 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
   }
   public void pedir(View view) {
+    if (mapa.getMyLocation() != null) {
+      JSONObject miubicacion = new JSONObject();
+      try {
+        miubicacion.put("latitude", mapa.getMyLocation().getLatitude());
+        miubicacion.put("longitude", mapa.getMyLocation().getLongitude());
+      } catch (JSONException e) {
+        Log.e("JSONExceptionPresenter", e.toString());
+      }
+      mSocket.emit("pedirtaxi", miubicacion, new Ack() {
+        @Override
+        public void call(Object... args) {
+          String res = (String) args[0];
+          if (res.equals("OK")) Log.i("mimensaje", "Se envio correctamente");
+          else Log.i("mimensaje", "Hubo error en el envio");
+        }
+      });
+    }
+    else
+      Toast.makeText(this,"no se ha encontrado su ubicación",Toast.LENGTH_SHORT).show();
   }
 }
